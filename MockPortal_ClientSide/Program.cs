8388617589731eeff48498bf00ede0a77501b4e1
Andrew <andrew@sitestudio.co>
2023-05-11 19:28:35 +1000

    Update Program.cs
    dummy change

--- MockPortal_ClientSide/Program.cs
+++ MockPortal_ClientSide/Program.cs
@@ -3,6 +3,7 @@ using Microsoft.AspNetCore.Components.WebAssembly.Hosting;
 using Microsoft.Extensions.DependencyInjection;
 using System;
 using System.Net.Http;
+
 using System.Threading.Tasks;
 
 namespace MockPortal_ClientSide
