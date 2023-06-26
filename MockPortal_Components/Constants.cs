49c6dd0424c5627da0c2ed394542ba374028b65b
Andrew McGrath <andrew_mcgrath@outlook.com>
2023-06-27 08:44:12 +1000

    [deploy] innocuous change

--- MockPortal_Components/Constants.cs
+++ MockPortal_Components/Constants.cs
@@ -8,7 +8,7 @@ namespace MockPortal_Components
     public class Constants
     {
         public const string ApiBaseUrl = "https://mockaco.azurewebsites.net/";
-            //= "http://localhost:5010/";
+        //= "http://localhost:5010/";
 
         public const string UrlPrefix = "/mocks"; // <= for azure deployment to inside of qed-developerportal
             // "";  // <= for localhost
