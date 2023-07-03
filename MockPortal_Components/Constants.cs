8ae9817e4325729ffc511c104777c0fe00810c20
Andrew McGrath <andrew_mcgrath@outlook.com>
2023-07-03 20:47:08 +1000

    [deploy] point to azurewebsites mockaco

--- MockPortal_Components/Constants.cs
+++ MockPortal_Components/Constants.cs
@@ -7,8 +7,8 @@ namespace MockPortal_Components
 {
     public class Constants
     {
-        public const string ApiBaseUrl //= "https://mockaco.azurewebsites.net/";
-         = "http://localhost:5010/";
+        public const string ApiBaseUrl = "https://mockaco.azurewebsites.net/";
+         // = "http://localhost:5010/";
 
         public const string UrlPrefix //= "/mocks"; // <= for azure deployment to inside of qed-developerportal
              = ""; // <= for localhost
