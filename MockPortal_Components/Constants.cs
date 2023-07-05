807d2f6d8ac6799a042982845b3cd8e51e322334
Andrew McGrath <andrew_mcgrath@outlook.com>
2023-07-05 13:36:40 +1000

    [deploy] use Route Attribute instead of @page directive

--- MockPortal_Components/Constants.cs
+++ MockPortal_Components/Constants.cs
@@ -10,7 +10,13 @@ namespace MockPortal_Components
         public const string ApiBaseUrl = "https://mockaco.azurewebsites.net/";
          // = "http://localhost:5010/";
 
-        public const string UrlPrefix //= "/mocks"; // <= for azure deployment to inside of qed-developerportal
-             = ""; // <= for localhost
+        public const string UrlPrefix = "/mocks"; // <= for azure deployment to inside of qed-developerportal
+                                      //            = ""; // <= for localhost
+
+        public const string IndexPageUrl = UrlPrefix + "/";
+        public const string ApiProductsPageUrl = UrlPrefix + "/apiproducts";
+        public const string ApiProductMethodsPageUrl = UrlPrefix + "/apiproductmethods/{groupcode}";
+        public const string ApiProductMethodTestPageUrl = UrlPrefix + "/apiproductmethodtest/{groupcode}/{id}";
+
     }
 }
