15c641e4e90124de96f1feb7197a5b6ac7495900
Andrew McGrath <andrew_mcgrath@outlook.com>
2023-07-05 15:23:49 +1000

    [deploy] trying with / for @page on index.razor

--- MockPortal_Components/Constants.cs
+++ MockPortal_Components/Constants.cs
@@ -10,7 +10,7 @@ namespace MockPortal_Components
         public const string ApiBaseUrl = "https://mockaco.azurewebsites.net/";
          // = "http://localhost:5010/";
 
-        public const string UrlPrefix = "/mocks"; // <= for azure deployment to inside of qed-developerportal
+        public const string UrlPrefix = ""; // <= for azure deployment to inside of qed-developerportal
                                       //            = ""; // <= for localhost
 
         public const string IndexPageUrl = UrlPrefix + "/";
