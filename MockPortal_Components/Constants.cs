d668114242a8896153a9159fd89a7982844f606f
Andrew McGrath <andrew_mcgrath@outlook.com>
2023-07-10 14:17:14 +1000

    [deploy] Add Response Layout to Test page

--- MockPortal_Components/Constants.cs
+++ MockPortal_Components/Constants.cs
@@ -7,15 +7,17 @@ namespace MockPortal_Components
 {
     public class Constants
     {
-        public const string ApiBaseUrl = "https://mockaco.azurewebsites.net/";
-            //= "http://localhost:5010/";
+        public const string ApiBaseUrl // = "https://mockaco.azurewebsites.net/";
+            = "http://localhost:5010/";
 
-        public const string UrlPrefix = "/mocks"; // <= for azure deployment to inside of qed-developerportal
-                                      //= ""; // <= for localhost
+        public const string UrlPrefix // = "/mocks"; // <= for azure deployment to inside of qed-developerportal
+                                      = ""; // <= for localhost
 
         public const string IndexPageUrl = UrlPrefix + "/";
         public const string ApiProductsPageUrl = UrlPrefix + "/apiproducts";
         public const string ApiProductMethodsPageUrl = UrlPrefix + "/apiproductmethods/{groupcode}";
         public const string ApiProductMethodTestPageUrl = UrlPrefix + "/apiproductmethodtest/{groupcode}/{id}";
+
+        public const string NewLine = "\r\n";
     }
 }
---
+++ MockPortal_Components/DataTypes/MethodParameter.cs
@@ -0,0 +1,13 @@
+﻿using System;
+namespace MockPortal_Components.DataTypes
+{
+	public class MethodParameter
+	{
+        public string Name { get; set; }
+        public string DataType { get; set; }
+        public bool Required { get; set; }
+        public string Description { get; set; }
+     
+    }
+}
+
